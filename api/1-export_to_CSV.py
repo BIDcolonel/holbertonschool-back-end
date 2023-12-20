46e577733d8c9f5770d48417186c2cb0f741b1ab
BIDcolonel <ptitofdu89@live.fr>
2023-12-20 11:44:43 +0100

    here we go again

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script that display infos for a given employee"""
+"""Script that save in csv infos from a given employee"""
 
 import csv
 import requests
@@ -10,64 +10,39 @@ API_URL = 'https://jsonplaceholder.typicode.com'
 
 
 def get_user_info(user_id):
-    user_request = requests.get(f'{API_URL}/users/{user_id}')
-    user_data = user_request.json()
-    return user_data
+    user_info = requests.get(f'{API_URL}/users/{user_id}').json()
+    return user_info
 
 
-def get_todo_list(user_id):
-    todo_list_request = requests.get(f"{API_URL}/todos?userId={user_id}")
-    todo_list_data = todo_list_request.json()
-    return todo_list_data
+def get_user_tasks(user_id):
+    tasks = requests.get(f"{API_URL}/todos?userId={user_id}").json()
+    return tasks
 
 
-def get_completed_tasks(todo_list_data):
-    completed_tasks = [task for task in todo_list_data if task['completed']]
-    return completed_tasks
-
-
-def display_employee_info(user_data, completed_tasks, total_tasks):
-    user_name = user_data["name"]
-    len_completed_tasks = len(completed_tasks)
-
-    print("Employee {} is done with tasks({}/{}):".format(
-        user_name,
-        len_completed_tasks,
-        total_tasks))
-
-    for task in completed_tasks:
-        print(f"\t {task['title']}")
-
-
-def export_to_csv(user_data, completed_tasks, filename):
+def export_to_csv(user, tasks, filename):
     with open(filename, mode='w') as csv_file:
         csv_writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
-        csv_writer.writerow(['User ID', 'User Name',
-                             'Completed', 'Task Title'])
 
-        for task in completed_tasks:
+        for task in tasks:
             csv_writer.writerow([
-                user_data['id'],
-                user_data['name'],
+                user['id'],
+                user['username'],
                 task['completed'],
                 task['title']
             ])
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <user_id>")
+    if len(sys.argv) < 2:
+        print("Please provide the user ID as an argument.")
         sys.exit(1)
 
     user_id = sys.argv[1]
 
     user_info = get_user_info(user_id)
-    todo_list = get_todo_list(user_id)
-    completed_tasks = get_completed_tasks(todo_list)
-    total_tasks = len(todo_list)
-
-    display_employee_info(user_info, completed_tasks, total_tasks)
+    user_tasks = get_user_tasks(user_id)
 
     csv_filename = f"{user_id}.csv"
-    export_to_csv(user_info, completed_tasks, csv_filename)
+    export_to_csv(user_info, user_tasks, csv_filename)
+
     print(f"Data has been exported to {csv_filename}")
